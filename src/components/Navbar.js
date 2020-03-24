9393b5a85f34ad4600216bf7fafc4d98c35a9412
Hasan <hasan.akkas@web.de>
2020-03-24 18:45:20 +0100

    some corrections

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap';
-
+import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <div style={{width: '100%'}} className="bg-light">
+        <div style={{width: '100%'}} className="bg-dark">
             <Navbar bg="dark" expand="lg" className="container text-white" variant="dark">
                 <Navbar.Brand href="/">COVID-19</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Nav.Link href="/">HOME</Nav.Link>
-                    <Nav.Link href="/country">SEARCH BY COUNTRY</Nav.Link>
+                    <Link className="text-light mr-3 link" to="/">HOME</Link>
+                    <Link className="text-light mr-3 link" to="/country">SEARCH BY COUNTRY</Link>
                     
                 </Nav>
                 </Navbar.Collapse>
